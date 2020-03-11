c48cb63341eabefb7846b0eef35d0ccf35523c14
HoHUng <hung.homanh.14@gmail.com>
2020-03-11 20:38:56 +0700

    'update'

--- Fundametals/session7/homework/optional.js
+++ Fundametals/session7/homework/optional.js
@@ -1,27 +1,64 @@
+let rowIndex,table = document.getElementById('bang')
+let inputF = document.getElementById('Firstname')
+let inputL = document.getElementById('Lastname')
+let inputAge = document.getElementById('Age')
+function checkEntryInput()
+{
+    let check = false
+    if(inputF.value=="")
+    {
+        alert("First name  cannot be entry")
+        check = true
+    }
+    else if(inputL.value=="")
+    {
+        alert("Last name  cannot be entry")
+        check = true
+    }
+    else if(inputAge.value=="")
+    {
+        alert("Age cannot be entry")
+        check = true
+    }
+    console.log(check)
+    return check
+}
 function addHtmlTalbeRow(){
-    let table = document.getElementById('bang')
-    var newRow = table.insertRow(table.length),
-    cell1 = newRow.insertCell(0),
-    cell2 = newRow.insertCell(1),
-    cell3 = newRow.insertCell(2)
-    let inputstt = document.getElementById('Firstname').value
-    let inputname = document.getElementById('Lastname').value
-    let inputAge = document.getElementById('Age').value
-    console.dir(table)
-    cell1.innerHTML = inputstt
-    cell2.innerHTML = inputname
-    cell3.innerHTML = inputAge
-
-    selectTedRowInput()
+    if(!checkEntryInput())
+    {
+        var newRow = table.insertRow(table.length),
+        cell1 = newRow.insertCell(0),//thêm cột 1
+        cell2 = newRow.insertCell(1),//thêm cột 2
+        cell3 = newRow.insertCell(2)//thêm cột 3
+        console.dir(table)
+        cell1.innerHTML = inputF.value//gán cột một có dữ liệu từ inputF
+        cell2.innerHTML = inputL.value//gán cột một có dữ liệu từ inputL
+        cell3.innerHTML = inputAge.value//gán cột một có dữ liệu từ inputAge
+    }
 }
 function selectTedRowInput(){
-    let rowIndex,table = document.getElementById('bang')
     for(let i = 1;i<table.rows.length;i++)
     {
         table.rows[i].onclick = function()
         {
-            rowIndex = this.rowIndex
+            console.dir(table)
+            rowIndex = this.rowIndex//trỏ vào thứ tự index thẻ tr
             console.log(rowIndex)
+            document.getElementById("Firstname").value = this.cells[0].innerHTML
+            document.getElementById("Lastname").value = this.cells[1].innerHTML
+            document.getElementById("Age").value = this.cells[2].innerHTML
         }
     }
 }
+selectTedRowInput()
+function editHtmlTedRow()
+{
+    table.rows[rowIndex].cells[0].innerHTML = inputF.value
+    table.rows[rowIndex].cells[1].innerHTML = inputL.value
+    table.rows[rowIndex].cells[2].innerHTML = inputAge.value
+}
+function deleteSelectTedRow()
+{
+    table.deleteRow(rowIndex)
+}
+
